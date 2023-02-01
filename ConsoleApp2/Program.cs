4fab5451f10e41efd909e212c62ac32c63cbb0d3
Blazingninja123 <118309706+Blazingninja123@users.noreply.github.com>
2023-02-01 13:34:36 -0600

    Math Files

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MyVar = 3;
+            
+            
+            Console.WriteLine(-1 + 4 * MyVar);
+
+            Console.WriteLine((35+5) % MyVar);
+
+            Console.WriteLine(14 + -4 * 6 / 12);
+
+            Console.WriteLine(2 + 12 / 6 * 1 - MyVar % 2);
+
+            if (MyVar * MyVar < 10)
+            Console.WriteLine("My variable is less than 10 ");
+
+            else
+            Console.WriteLine("My variable is greater than 10");
+
+        }
+    }
+}
+
